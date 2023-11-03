3482ed9ae63d7ba413a1f88a14c877feb2d9c0a1
Ellis Nielsen <thecakebecamethelie@yahoo.com>
2023-11-03 02:04:25 -0500

    Updated: shell scripts for handling shared lib

--- cmake_get_dependencies.sh
+++ cmake_get_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt install glfw freetype dbus libxkbcommon wayland libglvnd
+sudo apt install glfw libfreetype6-dev libdbus-1-dev libxkbcommon-dev libwayland-dev libglvnd-dev libcapstone-dev
