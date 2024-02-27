3687da5f69359c1fd08ebbe53262852161fefe96
Ellis Nielsen <thecakebecamethelie@yahoo.com>
2024-02-27 13:26:18 -0600

    add cmake to install command

--- cmake_get_dependencies.sh
+++ cmake_get_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt install glfw libfreetype6-dev libdbus-1-dev libxkbcommon-dev libwayland-dev libglvnd-dev libcapstone-dev
+sudo apt install glfw libfreetype6-dev libdbus-1-dev libxkbcommon-dev libwayland-dev libglvnd-dev libcapstone-dev cmake
